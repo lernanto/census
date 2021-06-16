dbfeb4a3c7f25a41658fc7a752f63454a4c4229c
黄艺华 <lernanto.wong@gmail.com>
2021-06-16 17:48:16 +0800

    make crawler more general

--- crawl.py
+++ crawl.py
@@ -6,49 +6,60 @@
 __author__ = 'Edward Wong <lernanto.wong@gmail.com>'
 
 
+import sys
 import logging
+import configparser
 import os
 import requests
 import lxml.etree
+import re
 import time
 
 
-def process(node, directory, url_prefix, delay=None):
-    '''递归地处理网页中地章节列表，并下载数据.'''
+def process(node, directory, url_prefix, dry_run=False, delay=None):
+    '''递归地处理网页目录中的章节列表，并下载数据.'''
 
     # 先创建本目录
     logging.info('make directory {}'.format(directory))
-    os.makedirs(directory, exist_ok=True)
+    if not dry_run:
+        os.makedirs(directory, exist_ok=True)
 
     # 递归处理子目录
     title = '未命名'
     for c in node.getchildren():
-        if c.tag == 'li':
-            anchors = c.xpath('a')
-            if len(anchors) > 0:
-                # 含有超链接的是叶节点，下载
-                href = anchors[0].get('href')
-                if href:
-                    url = '{}/{}'.format(url_prefix, href)
+        anchors = c.xpath('.//a')
+        if len(anchors) == 0:
+            # 不含超链接，可能是章节标题
+            text = re.sub(r'[:/\\?.\s]+', ' ', ''.join(c.itertext())).strip()
+            if text:
+                title = text
 
-                    # 标题
-                    title = anchors[0].text.strip()
-                    if title:
-                        _, ext = os.path.splitext(href)
-                        if not ext:
-                            ext = 'xsl'
-                        fname = '{}{}'.format(title, ext)
-                    else:
-                        fname = href.split('/')[-1]
+        elif len(anchors) == 1:
+            # 含有一个超链接，叶节点，下载
+            href = anchors[0].get('href')
+            if href:
+                url = '{}/{}'.format(url_prefix, href)
 
-                    path = os.path.join(directory, fname)
-                    # TODO: 异常情况下可能出现新文件和已有文件重名，暂不处理
+                # 标题，尽量提取章节标题，如果没有，使用链接文件名
+                text = re.sub(r'[:/\\?.\s]+', ' ', anchors[0].text).strip()
+                if text:
+                    _, ext = os.path.splitext(href)
+                    if not ext:
+                        ext = 'xls'
+                    fname = '{}{}'.format(text, ext)
+                else:
+                    fname = href.split('/')[-1]
 
-                    logging.info('download {} -> {}'.format(url, path))
+                path = os.path.join(directory, fname)
+                # TODO: 异常情况下可能出现新文件和已有文件重名，暂不处理
+
+                logging.info('download {} -> {}'.format(url, path))
+                if not dry_run:
                     rsp = requests.get(url)
                     if rsp.status_code == 200:
                         with open(path, 'wb') as f:
-                            f.write(rsp.raw.data)
+                            for chunk in rsp.iter_content(chunk_size=512):
+                                f.write(chunk)
                     else:
                         logging.error('{}: {}'.format(rsp.status_code, rsp.reason))
 
@@ -56,26 +67,39 @@ def process(node, directory, url_prefix, delay=None):
                     if delay > 0:
                         time.sleep(delay)
 
-            else:
-                # 非叶节点，章节标题
-                title = c.text.strip()
+        else:
+            # 含有多个超链接，子目录，递归处理
+            process(c, os.path.join(directory, title), url_prefix, dry_run, delay)
 
-        elif c.tag == 'ul':
-            # 子目录
-            process(c, os.path.join(directory, title), url_prefix, delay)
-
-
-url = 'http://www.stats.gov.cn/tjsj/pcsj/rkpc/6rp/lefte.htm'
-directory = '中国2010年人口普查资料'
-url_prefix = '/'.join(url.split('/')[:-1])
 
 logging.getLogger().setLevel(logging.INFO)
+
+config = configparser.ConfigParser()
+config.read_string('[DEFAULT]\n' + open(sys.argv[1], encoding='utf8').read())
+config = config['DEFAULT']
+
+for k, v in config.items():
+    logging.info('{} = {}'.format(k, v))
+
+url = config.get('url')
+xpath = config.get('xpath')
+encoding = config.get('encoding', 'GB18030')
+output = config.get('output', '.')
+dry_run = config.getboolean('dry_run', False)
+delay = config.getfloat('delay')
+
+if not url or not xpath:
+    logging.error('must set url and xpath')
+    sys.exit(1)
+
+url_prefix = '/'.join(url.split('/')[:-1])
+
 logging.info('start crawling {}'.format(url))
 
 rsp = requests.get(url)
-rsp.encoding = 'GB18030'
+rsp.encoding = encoding
 doc = lxml.etree.HTML(rsp.text)
-root = doc.xpath('//table/tbody/center/tr[last()]/th/ul')[0]
+root = doc.xpath(xpath)[0]
 
-process(root, directory, url_prefix, 0.1)
+process(root, output, url_prefix, dry_run=dry_run, delay=delay)
 logging.info('done')
